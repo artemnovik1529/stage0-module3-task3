759224551010fc5f1ec66db468ef69380a34cf89
Artem Novik <mr.artnov@mail.ru>
2022-10-19 23:44:16 +0300

    feat: update task

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -6,9 +6,9 @@ public class ReassigningValues {
         int second = 10;
         int third = 100;
         System.out.println(first + "\n" + second + "\n" + third);
-        int linkToFirst = first = 15;
-        int linkToSecond = second = 6;
-        int linkToThird = third = 4;
+        int linkToFirst =  15;
+        int linkToSecond =  6;
+        int linkToThird =  4;
         System.out.println(linkToFirst + "\n" + linkToSecond + "\n" + linkToThird);
         System.out.println(first + "\n" + second + "\n" + third);
     }
